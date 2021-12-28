b5a5db4b3532d8cafb432d0d2baa211321f74d19
Mohammad Banisaeid <smbl64@gmail.com>
2021-12-28 15:54:30 +0100

    Create base models for accessing the db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"banisaeid.com/letsgo/pkg/models/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "Address to listen to")
+	dsn := flag.String("dsn", "web:web@/snippetbox?parseTime=true", "MySQL connection string (DSN)")
 	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
-		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
-		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		infoLog:  infoLog,
+		errorLog: errorLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	app.infoLog.Printf("Starting server on %s\n", *addr)
@@ -28,6 +43,19 @@ func main() {
 		ErrorLog: app.errorLog,
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	app.errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
