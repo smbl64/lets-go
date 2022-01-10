00a73d825f61521e2c8e262044e17dd9bb97d4fc
Mohammad Banisaeid <smbl64@gmail.com>
2022-01-10 13:58:02 +0100

    Add csrf protection to forms

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/justinas/nosurf"
 )
 
 func (app *application) serveError(w http.ResponseWriter, err error) {
@@ -49,6 +51,7 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
 	td.CurrentYear = time.Now().Year()
 	td.Flash = app.session.PopString(r, "flash")
 	td.IsAuthenticated = app.isAuthenticated(r)
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
