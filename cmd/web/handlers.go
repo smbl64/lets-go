1f53c33153bb28a57b07d101e77d37fcbe965f67
Mohammad Banisaeid <smbl64@gmail.com>
2022-01-10 15:10:49 +0100

    Add test for a ping handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -139,3 +139,7 @@ func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
 	app.session.Put(r, "flash", "You have been successfully logged out")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	rr := httptest.NewRecorder()
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ping(rr, r)
+	rs := rr.Result()
+	if rs.StatusCode != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("want %q; got %q", "OK", string(body))
+	}
+}
